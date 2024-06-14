568f3b4f5b1ddf11e772a5dfeed39dde2ab54126
Daniela Vornic <daniela.vornic@gmail.com>
2024-06-14 18:42:05 +0300

    feat: init packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#edeaff",
+      100: "#ccc1f4",
+      200: "#ac99e7",
+      300: "#8d71dc",
+      400: "#7248d0",
+      500: "#502fb7",
+      600: "#37248f",
+      700: "#211968",
+      800: "#110f40",
+      900: "#04031b"
+    }
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
 }
