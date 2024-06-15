739898588fff4f1f3fe40abb97a54fd88861916e
maria <118808220+yum3lo@users.noreply.github.com>
2024-06-15 18:51:16 +0300

    Update [slug].tsx

--- src/pages/glossary/[slug].tsx
+++ src/pages/glossary/[slug].tsx
@@ -4,6 +4,7 @@ import glossaryTerms from '@/data/terms.json';
 import glossaryByLetter from '@/data/glossaryTerms.json';
 import { Box, Heading, Image, Text, Stack, List, ListItem, Button, Flex, Divider } from '@chakra-ui/react';
 import { Link } from "@chakra-ui/next-js";
+import { Layout } from '@/components';
 
 type GlossaryTermsType = {
   [key: string]: string[];
@@ -23,56 +24,58 @@ const TermPage = () => {
   const [selectedTerm, setSelectedTerm] = React.useState<string | null>(null);
 
   return (
-		<Box>
-      {/* Header */}
-      <Box as="header" p={4} textAlign="center">
-        <Heading as="h1" color="brand.900">Glossary</Heading>
-      </Box>
-			<Flex>
-				{/* Left Section: Glossary List */}
-				<Box flex="1" p={4} maxW="26%" maxH="80vh" overflowY="auto" overflowX="hidden" className="custom-scrollbar">
-					<Divider mb={4} />
-					<Stack spacing={4}>
-						{Object.keys(glossaryByLetter).map((letter) => (
-							!selectedTerm || selectedTerm === letter ? (
-								<Box key={letter}>
-									<Heading as="h2" size="md" mb={2} color="brand.700">{letter}</Heading>
-									<List justifyContent="center" alignItems="center">
-										{glossaryByLetter[letter].map((term) => (
-											<ListItem key={term}>
-												<Link 
-													href={`/glossary/${encodeURIComponent(term)}`} 
-													borderRadius="md"
-                          margin="0 8px"
-                          padding="4px 6px"
-                          _hover={{ 
-                            background: "#ccc1f4",
-                          }}
-                          textAlign="center" // Center align the link content
-                          whiteSpace="nowrap" // Prevent wrapping of long terms
-                          minWidth="0" // Ensure terms can shrink if needed
-												>{term}</Link>
-											</ListItem>
-										))}
-									</List>
-								</Box>
-							) : null
-						))}
-					</Stack>
+		<Layout title="Glossary">
+			<Box>
+				{/* Header */}
+				<Box as="header" p={4} textAlign="center">
+					<Heading as="h1" color="brand.900">Glossary</Heading>
 				</Box>
-
-				{/* Right Section: Selected Term Details */}
-				<Box flex="1" p={4} mr="4">
-					<Heading as="h1" mb={4} textAlign="center" color="brand.900">
-						{slug}
-					</Heading>
-					<Box maxW="400px" mx="auto" mb={4}>
-						<Image src={termInfo.image} alt={`${slug} image`} />
+				<Flex>
+					{/* Left Section: Glossary List */}
+					<Box flex="1" p={4} maxW="26%" maxH="80vh" overflowY="auto" overflowX="hidden" className="custom-scrollbar">
+						<Divider mb={4} />
+						<Stack spacing={4}>
+							{Object.keys(glossaryByLetter).map((letter) => (
+								!selectedTerm || selectedTerm === letter ? (
+									<Box key={letter}>
+										<Heading as="h2" size="md" mb={2} color="brand.700">{letter}</Heading>
+										<List justifyContent="center" alignItems="center">
+											{glossaryByLetter[letter].map((term) => (
+												<ListItem key={term}>
+													<Link 
+														href={`/glossary/${encodeURIComponent(term)}`} 
+														borderRadius="md"
+														margin="0 8px"
+														padding="4px 6px"
+														_hover={{ 
+															background: "#ccc1f4",
+														}}
+														textAlign="center" // Center align the link content
+														whiteSpace="nowrap" // Prevent wrapping of long terms
+														minWidth="0" // Ensure terms can shrink if needed
+													>{term}</Link>
+												</ListItem>
+											))}
+										</List>
+									</Box>
+								) : null
+							))}
+						</Stack>
 					</Box>
-					<Text textAlign="justify">{termInfo.explanation}</Text>
-				</Box>
-			</Flex>
-		</Box>
+
+					{/* Right Section: Selected Term Details */}
+					<Box flex="1" p={4} mr="4">
+						<Heading as="h1" mb={4} textAlign="center" color="brand.900">
+							{slug}
+						</Heading>
+						<Box maxW="400px" mx="auto" mb={4}>
+							<Image src={termInfo.image} alt={`${slug} image`} />
+						</Box>
+						<Text textAlign="justify">{termInfo.explanation}</Text>
+					</Box>
+				</Flex>
+			</Box>
+		</Layout>
   );
 };
 
