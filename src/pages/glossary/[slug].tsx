305b725b9bf8b00cba226a784c54dced4d4967ab
maria <118808220+yum3lo@users.noreply.github.com>
2024-06-15 17:54:11 +0300

    glossary

--- src/pages/glossary/[slug].tsx
+++ src/pages/glossary/[slug].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import React from 'react';
-import glossaryTerms from '@/data/glossaryTerms.json';
+import glossaryTerms from '@/data/terms.json';
+import glossaryByLetter from '@/data/glossaryTerms.json';
 import { Box, Heading, Image, Text, Stack, List, ListItem, Button, Flex, Divider } from '@chakra-ui/react';
 import { Link } from "@chakra-ui/next-js";
 
@@ -12,13 +13,16 @@ const TermPage = () => {
   const router = useRouter();
   const { slug } = router.query;
 
+  // Ensure slug is a string and exists in glossaryTerms
   if (typeof slug !== 'string' || !(slug in glossaryTerms)) {
+		console.log('Slug is not a string:', slug);
     return <div>Term not found</div>;
   }
 
+  // Extract the term info from glossaryTerms based on the slug
   const termInfo = glossaryTerms[slug][0]; // Assuming each term has only one item in the array
-
-	const [selectedTerm, setSelectedTerm] = React.useState<string | null>(null);
+	console.log('Term Info:', termInfo);
+  const [selectedTerm, setSelectedTerm] = React.useState<string | null>(null);
 
   const handleTermClick = (term: string) => {
     setSelectedTerm(term);
@@ -46,12 +50,12 @@ const TermPage = () => {
         </Flex>
         <Divider mb={4} />
         <Stack spacing={4}>
-          {Object.keys(glossaryTerms).map((letter) => (
+          {Object.keys(glossaryByLetter).map((letter) => (
             !selectedTerm || selectedTerm === letter ? (
               <Box key={letter}>
                 <Heading as="h2" size="md" mb={2}>{letter}</Heading>
                 <List>
-                  {glossaryTerms[letter].map((term) => (
+                  {glossaryByLetter[letter].map((term) => (
                     <ListItem key={term}>
                       <Link href={`/glossary/${encodeURIComponent(term)}`} _hover={{ textDecoration: "underline" }}>{term}</Link>
                     </ListItem>
