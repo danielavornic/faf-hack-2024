156c74dc86b71d71fbc57475fd345714615c2bc7
maria <118808220+yum3lo@users.noreply.github.com>
2024-06-15 18:49:44 +0300

    glossary scrollbar

--- src/pages/glossary/[slug].tsx
+++ src/pages/glossary/[slug].tsx
@@ -15,69 +15,64 @@ const TermPage = () => {
 
   // Ensure slug is a string and exists in glossaryTerms
   if (typeof slug !== 'string' || !(slug in glossaryTerms)) {
-		console.log('Slug is not a string:', slug);
     return <div>Term not found</div>;
   }
 
   // Extract the term info from glossaryTerms based on the slug
   const termInfo = glossaryTerms[slug][0]; // Assuming each term has only one item in the array
-	console.log('Term Info:', termInfo);
   const [selectedTerm, setSelectedTerm] = React.useState<string | null>(null);
 
-  const handleTermClick = (term: string) => {
-    setSelectedTerm(term);
-  };
-
-  const showAllTerms = () => {
-    setSelectedTerm(null);
-  };
-
   return (
-    <Flex>
-      {/* Left Section: Glossary List */}
-      <Box flex="1" p={4}>
-        <Heading as="h1" mb={4} textAlign="center">Glossary</Heading>
-        <Flex wrap="wrap" justifyContent="center">
-          <Button onClick={showAllTerms} mr={1} mb={2}>Show All</Button>
-          {/* Render '0-9' button separately */}
-          <Button onClick={() => handleTermClick('0-9')} mr={1} mb={2}>0-9</Button>
-          {/* Render alphabetical buttons dynamically */}
-          {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((letter) => (
-            <Button key={letter} onClick={() => handleTermClick(letter)} mr={1} mb={2}>
-              {letter}
-            </Button>
-          ))}
-        </Flex>
-        <Divider mb={4} />
-        <Stack spacing={4}>
-          {Object.keys(glossaryByLetter).map((letter) => (
-            !selectedTerm || selectedTerm === letter ? (
-              <Box key={letter}>
-                <Heading as="h2" size="md" mb={2}>{letter}</Heading>
-                <List>
-                  {glossaryByLetter[letter].map((term) => (
-                    <ListItem key={term}>
-                      <Link href={`/glossary/${encodeURIComponent(term)}`} _hover={{ textDecoration: "underline" }}>{term}</Link>
-                    </ListItem>
-                  ))}
-                </List>
-              </Box>
-            ) : null
-          ))}
-        </Stack>
+		<Box>
+      {/* Header */}
+      <Box as="header" p={4} textAlign="center">
+        <Heading as="h1" color="brand.900">Glossary</Heading>
       </Box>
+			<Flex>
+				{/* Left Section: Glossary List */}
+				<Box flex="1" p={4} maxW="26%" maxH="80vh" overflowY="auto" overflowX="hidden" className="custom-scrollbar">
+					<Divider mb={4} />
+					<Stack spacing={4}>
+						{Object.keys(glossaryByLetter).map((letter) => (
+							!selectedTerm || selectedTerm === letter ? (
+								<Box key={letter}>
+									<Heading as="h2" size="md" mb={2} color="brand.700">{letter}</Heading>
+									<List justifyContent="center" alignItems="center">
+										{glossaryByLetter[letter].map((term) => (
+											<ListItem key={term}>
+												<Link 
+													href={`/glossary/${encodeURIComponent(term)}`} 
+													borderRadius="md"
+                          margin="0 8px"
+                          padding="4px 6px"
+                          _hover={{ 
+                            background: "#ccc1f4",
+                          }}
+                          textAlign="center" // Center align the link content
+                          whiteSpace="nowrap" // Prevent wrapping of long terms
+                          minWidth="0" // Ensure terms can shrink if needed
+												>{term}</Link>
+											</ListItem>
+										))}
+									</List>
+								</Box>
+							) : null
+						))}
+					</Stack>
+				</Box>
 
-      {/* Right Section: Selected Term Details */}
-      <Box flex="1" p={4}>
-        <Heading as="h1" mb={4} textAlign="center">
-          {slug}
-        </Heading>
-        <Box maxW="400px" mx="auto" mb={4}>
-          <Image src={termInfo.image} alt={`${slug} image`} />
-        </Box>
-        <Text>{termInfo.explanation}</Text>
-      </Box>
-    </Flex>
+				{/* Right Section: Selected Term Details */}
+				<Box flex="1" p={4} mr="4">
+					<Heading as="h1" mb={4} textAlign="center" color="brand.900">
+						{slug}
+					</Heading>
+					<Box maxW="400px" mx="auto" mb={4}>
+						<Image src={termInfo.image} alt={`${slug} image`} />
+					</Box>
+					<Text textAlign="justify">{termInfo.explanation}</Text>
+				</Box>
+			</Flex>
+		</Box>
   );
 };
 
